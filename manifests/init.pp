23cd1b576965358be14eadcb46e2581f9b86b0a0
Marcus Campbell <marcus.anthony.campbell@gmail.com>
2015-06-23 11:11:25 +0200

    fixing pdnsd not starting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,18 @@
 #
 class pdnsd (
-  $config            = $pdnsd::params::config,
-  $config_template   = $pdnsd::params::config_template,
-  $package_ensure    = $pdnsd::params::package_ensure,
-  $package_name      = $pdnsd::params::package_name,
-  $service_enable    = $pdnsd::params::service_enable,
-  $service_ensure    = $pdnsd::params::service_ensure,
-  $service_manage    = $pdnsd::params::service_manage,
-  $service_name      = $pdnsd::params::service_name,
-  $start_daemon      = $pdnsd::params::start_daemon,
-) inherits pdnsd::params {
+) {
+
+  $config_template    = hiera(pdnsd::config_template, 'pdnsd/pdnsd.conf.erb' )
+  $package_ensure     = hiera(pdnsd::package_ensure, 'present' )
+  $service_enable     = hiera(pdnsd::service_enable, true )
+  $service_ensure     = hiera(pdnsd::service_ensure, 'running' )
+  $service_manage     = hiera(pdnsd::service_manage, true )
+  $server_ip          = hiera(pdnsd::server_ip, '127.0.0.1' )
+
+  $perm_cache         = hiera(pdnsd::perm_cache, 16384)
+  $cache_dir          = hiera(pdnsd::cache_dir, '/var/cache/pdnsd')
+  $start_daemon       = hiera(pdnsd::start_daemon, 'yes')
+  $preferred_servers  = hiera_hash(pdnsd::preferred_servers, undef )
 
   validate_absolute_path($config)
   validate_string($config_template)
@@ -20,7 +23,50 @@ class pdnsd (
   validate_bool($service_manage)
   validate_string($service_name)
 
-  $preferred_servers  = hiera_hash(pdnsd::preferred_servers, undef )
+  case $::osfamily {
+    'Debian': {
+      $config          = '/etc/pdnsd.conf'
+      $package_name    = [ 'pdnsd' ]
+      $service_name    = 'pdnsd'
+    }
+    'RedHat': {
+      $config          = '/etc/pdnsd.conf'
+      $package_name    = [ 'pdnsd' ]
+      $service_name    = 'pdnsd'
+    }
+    'SuSE': {
+      $config          = '/etc/pdnsd.conf'
+      $package_name    = [ 'pdnsd' ]
+      $service_name    = 'pdnsd'
+    }
+    'FreeBSD': {
+      $config          = '/etc/pdnsd.conf'
+      $package_name    = ['net/pdnsd']
+      $service_name    = 'pdnsd'
+    }
+    'Archlinux': {
+      $config          = '/etc/pdnsd.conf'
+      $package_name    = [ 'pdnsd' ]
+      $service_name    = 'pdnsd'
+    }
+    'Linux': {
+      # Account for distributions that don't have $::osfamily specific settings.
+      case $::operatingsystem {
+        'Gentoo': {
+          $config          = '/etc/pdnsd.conf'
+          $package_name    = ['net-dns/pdnsd']
+          $service_name    = 'pdnsd'
+        }
+        default: {
+          fail("The ${module_name} module is not supported on an ${::operatingsystem} distribution.")
+        }
+      }
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
+    }
+  }
+
 
   include '::pdnsd::install'
   include '::pdnsd::config'
