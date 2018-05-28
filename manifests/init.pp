ae3af1516a364bf2a1d6f0c9385146e768b20385
mac <marcus.anthony.campbell@gmail.com>
2018-05-28 13:36:30 +0200

    .

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,42 +2,42 @@
 class pdnsd (
 ) {
 
-  $config_template    = hiera(pdnsd::config_template, 'pdnsd/pdnsd.conf.erb' )
-  $package_ensure     = hiera(pdnsd::package_ensure, 'present' )
-  $service_enable     = hiera(pdnsd::service_enable, true )
-  $service_ensure     = hiera(pdnsd::service_ensure, 'running' )
-  $service_manage     = hiera(pdnsd::service_manage, true )
-  $server_ip          = hiera(pdnsd::server_ip, '127.0.0.1' )
+  $config_template    = lookup(pdnsd::config_template, String, 'unique', 'pdnsd/pdnsd.conf.erb' )
+  $package_ensure     = lookup(pdnsd::package_ensure, String, 'unique', 'present' )
+  $service_enable     = lookup(pdnsd::service_enable, Boolean, 'unique', true )
+  $service_ensure     = lookup(pdnsd::service_ensure, String, 'unique', 'running' )
+  $service_manage     = lookup(pdnsd::service_manage, Boolean, 'unique', true )
+  $server_ip          = lookup(pdnsd::server_ip, String, 'unique', '127.0.0.1' )
 
-  $perm_cache         = hiera(pdnsd::perm_cache, 16384)
-  $cache_dir          = hiera(pdnsd::cache_dir, '/var/cache/pdnsd')
-  $start_daemon       = hiera(pdnsd::start_daemon, 'yes')
-  $preferred_servers  = hiera_hash(pdnsd::preferred_servers, undef )
+  $perm_cache         = lookup(pdnsd::perm_cache, Integer, 'unique', 16384)
+  $cache_dir          = lookup(pdnsd::cache_dir, String, 'unique', '/var/cache/pdnsd')
+  $start_daemon       = lookup(pdnsd::start_daemon, Tuple, 'unique', 'yes')
+  $preferred_servers  = lookup(pdnsd::preferred_servers, {merge => 'hash', default_value => undef}) #hash
 
   case $::osfamily {
     'Debian': {
       $config          = '/etc/pdnsd.conf'
-      $package_name    = [ 'pdnsd' ]
+      $package_name    = 'pdnsd'
       $service_name    = 'pdnsd'
     }
     'RedHat': {
       $config          = '/etc/pdnsd.conf'
-      $package_name    = [ 'pdnsd' ]
+      $package_name    = 'pdnsd'
       $service_name    = 'pdnsd'
     }
     'SuSE': {
       $config          = '/etc/pdnsd.conf'
-      $package_name    = [ 'pdnsd' ]
+      $package_name    = 'pdnsd'
       $service_name    = 'pdnsd'
     }
     'FreeBSD': {
       $config          = '/etc/pdnsd.conf'
-      $package_name    = ['net/pdnsd']
+      $package_name    = 'net/pdnsd'
       $service_name    = 'pdnsd'
     }
     'Archlinux': {
       $config          = '/etc/pdnsd.conf'
-      $package_name    = [ 'pdnsd' ]
+      $package_name    = 'pdnsd'
       $service_name    = 'pdnsd'
     }
     'Linux': {
@@ -45,7 +45,7 @@ class pdnsd (
       case $::operatingsystem {
         'Gentoo': {
           $config          = '/etc/pdnsd.conf'
-          $package_name    = ['net-dns/pdnsd']
+          $package_name    = 'net-dns/pdnsd'
           $service_name    = 'pdnsd'
         }
         default: {
@@ -61,7 +61,7 @@ class pdnsd (
   validate_absolute_path($config)
   validate_string($config_template)
   validate_string($package_ensure)
-  validate_array($package_name)
+  validate_string($package_name)
   validate_bool($service_enable)
   validate_string($service_ensure)
   validate_bool($service_manage)
