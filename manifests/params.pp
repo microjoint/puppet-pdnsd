82b7e4440491006c9f891060491fb031db1e9316
Marcus Campbell <marcus.campbell@williamhill.com>
2018-11-07 16:47:32 +0100

    let us start testing

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,16 @@
 #
 class pdnsd::params {
+  $config = undef
 
+  case $::osfamily {
+    'Debian': {
+      $package = {
+        'pdns-server' => {},
+      }
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an \
+      ${::osfamily} based system.")
+    }
+  }
 }
